8afb0fddc1aa26ddbf327e33e33efab1842a3eb4
TeetoeTurtle <60118294+TeetoeTurtle@users.noreply.github.com>
2023-03-23 09:33:23 -0500

    amongus yae miko items revision #1
    yae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae mikoyae miko

--- game/engine/items.py
+++ game/engine/items.py
@@ -4,6 +4,11 @@ class Item:
     def __init__(self, name, quantity: int=1) -> None:
         self.name = name
         self.quantity = quantity
+        self.desc="Description: your mother"
+    def get_desc(self):
+        return self.desc
+    def use_item(self):
+        return 0
     
     def __str__(self) -> str:
         if self.quantity == 1:
@@ -20,3 +25,52 @@ class Item:
 
     def __isub__(self, n: int) -> object:
         return type(self)(self.name, quantity - n)
+    class Melee_Weapon(Item):
+    damage=0
+    desc=""
+
+    def __init__(self, dmg, descr):
+        self.damage= (-1 * abs(dmg))
+        self.desc= descr
+    
+    def use_item(self):
+        return self.damage
+
+    def give_desc(self):
+        return self.desc
+
+
+class Range_Weapon(Item):
+    damage=0
+    desc=""
+    max_range = 0
+    current_range=0
+
+    def __init__(self, dmg, rango, descr):
+        self.damage= (-1 * abs(dmg))
+        self.desc= descr
+        self.max_range=rango
+    
+    def find_range(self, range):
+        self.current_range=range
+
+    def use_item(self):
+        if self.max_range >= self.current_range:
+            return self.damage
+        elif self.max_range<self.current_range:
+            print("out of range bozo kys")
+            return 0
+
+    def give_desc(self):
+        return self.desc
+
+    def get_range(self):
+        return self.rango
+
+class Potion(Item):
+    health_gain=0
+    
+    def __init__(self, hG):
+        self.health_gain=abs(hG)
+    def use_item(self):
+        return self.health_gain
