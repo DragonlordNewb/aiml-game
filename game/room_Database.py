c0b6e91f19c6c16a179f97d3b5288a554def6ad2
Noddley <102306461+Noddley@users.noreply.github.com>
2023-03-24 09:59:16 -0500

    Update room_Database.py

--- game/room_Database.py
+++ game/room_Database.py
@@ -10,7 +10,6 @@ def Roomshuffle():
     random.shuffle(Room_list)
 Roomshuffle()
 
-
 #RoomVars
 Current_room = Room_list[0]
 Room_north = Room_list[1]
@@ -19,7 +18,6 @@ Room_south = Room_list[3]
 Room_west = Room_list[4]
 Current_room_main = ''
 Player_res = ''
-
 
 print("You're in",Current_room)
 print("north-",Room_north)
@@ -30,21 +28,15 @@ print("west-",Room_west)
 #start of inputs test and the endless loop #chage to classes if possible?
 Player_res = input("Please select a side ")
 
-
-
 def room_selection(old_room, New_room):
     old_room = New_room
      
-
  #Add NLP ai Remove later
 def userinput(User_input):
     if User_input == "east":
         room_selection(Current_room, Room_east)
 
 userinput(Player_res)
-
-
-
 
 #tests remove later
 print("You're in",Current_room_main)
