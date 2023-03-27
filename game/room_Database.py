e2aac06682ab842c727f359b216eb3869e8e9ea7
Noddley <102306461+Noddley@users.noreply.github.com>
2023-03-27 09:58:57 -0500

    Update room_Database.py
    This is broke

--- game/room_Database.py
+++ game/room_Database.py
@@ -16,8 +16,9 @@ Room_north = Room_list[1]
 Room_east = Room_list[2]
 Room_south = Room_list[3]
 Room_west = Room_list[4]
-Current_room_main = ''
+Current_room_old = ''
 Player_res = ''
+hp = 3
 
 print("You're in",Current_room)
 print("north-",Room_north)
@@ -25,22 +26,47 @@ print("east-",Room_east)
 print("south-",Room_south)
 print("west-",Room_west)
 
+
+#add while loop
+
+
+
 #start of inputs test and the endless loop #chage to classes if possible?
 Player_res = input("Please select a side ")
 
-def room_selection(old_room, New_room):
-    old_room = New_room
-     
+def room_selection(side):
+    global Current_room
+    Current_room = side
+    oldRoom(Player_res)
+    
+
+   
  #Add NLP ai Remove later
 def userinput(User_input):
+    global Current_room_old
+    #add NLP
+    Current_room_old = Room_east
+    print(Current_room_old)
     if User_input == "east":
-        room_selection(Current_room, Room_east)
+        room_selection(Room_east)
+
+
+def oldRoom(user_input):
+    global Current_room_old
+    if user_input == 'east':
+        Current_room_old = Room_south
+        print(Room_south)
+
+
+
+
 
 userinput(Player_res)
 
 #tests remove later
-print("You're in",Current_room_main)
+print("You're in",Current_room)
 print("north-",Room_north)
 print("east-",Room_east)
 print("south-",Room_south)
 print("west-",Room_west)
+
