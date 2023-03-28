aa276bc0a78adda197e1652273e379cf4d4a2db0
stickbuggg <128164425+stickbuggg@users.noreply.github.com>
2023-03-28 09:53:36 -0500

    Update npcs.py
    added more stuff

--- game/engine/npcs.py
+++ game/engine/npcs.py
@@ -1,8 +1,22 @@
 import random
 
 class Npc:
-    def __init__(self, name):
+    def __init__(self, type, name, description, damage, defense, health, taken_damage, dead):
         self.name = name
+        self.type = type
+        self.description = description
+        self.damage = damage
+        self.defense = defense
+        self.health = health
+        self.taken_damage = taken_damage
+        self.dead = dead
+        dead = False
+    
+    def take_damage(self, health, taken_damage, dead)
+        health = health-taken_damage
+        if health == 0:
+            dead = True
+
   
 
 class Random(Npc):
@@ -15,4 +29,3 @@ class Random(Npc):
         action = actions[random.randint(0,len(actions))-1]
 
 class SearchTree(Npc):
-    
