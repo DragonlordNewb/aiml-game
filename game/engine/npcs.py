734d5c358ec6591b5b28baac9e1c766aeaecc4b8
stickbuggg <128164425+stickbuggg@users.noreply.github.com>
2023-03-28 09:38:42 -0500

    Create npcs.py
    not done at all lol.

---
+++ game/engine/npcs.py
@@ -0,0 +1,19 @@
+import random
+
+class Npc:
+    name = ''
+    def __init__(self, name):
+        self.name = name
+  
+
+class Random(Npc):
+    actions = []
+
+    def __init__(self, actions):
+        self.actions = actions
+    
+    def take_action(self, actions):
+        action = actions[random.randint(0,len(actions))-1]
+
+class SearchTree(Npc):
+    
