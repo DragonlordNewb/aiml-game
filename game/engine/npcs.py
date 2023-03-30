66fe9099c99fe703e4da36e8aa01eba963d72fb4
stickbuggg <128164425+stickbuggg@users.noreply.github.com>
2023-03-30 09:16:00 -0500

    Update npcs.py
    added more stuff

--- game/engine/npcs.py
+++ game/engine/npcs.py
@@ -1,7 +1,7 @@
 import random
 
 class Npc:
-    def __init__(self, type, name, description, damage, defense, health, taken_damage, dead):
+    def __init__(self, type, name, description, damage, defense, health, taken_damage, dead, current_enemy):
         self.name = name
         self.type = type
         self.description = description
@@ -10,12 +10,17 @@ class Npc:
         self.health = health
         self.taken_damage = taken_damage
         self.dead = dead
-        dead = False
+        self.current_enemy = current_enemy
     
     def take_damage(self, health, taken_damage, dead):
         health = health-taken_damage
         if health == 0:
             dead = True
+    
+    def deal_damage(self, damage, enemy):
+        enemy.health = enemy.health - damage
+        
+
 
   
 
