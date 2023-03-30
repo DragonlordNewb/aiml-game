cb0dcca664a500055b2db6dac4b97194404355d3
Noddley <102306461+Noddley@users.noreply.github.com>
2023-03-30 09:38:30 -0500

    Completed 2D Array On to bug fixing

--- game/room_Database.py
+++ game/room_Database.py
@@ -4,38 +4,59 @@ import random
 #from game import main_game
 #import NLP_database
 Room_list = ['Cabins','Kitchen','Pool','Fitnesscenter','Casino','Smokingarea','shoppingcenter','Captainsroom','The Bridge']
-Room_map = []
+temp_list = Room_list
+Room_map = [['','',''],['','',''],['','','']]
 #RoomSuffle system
 
-Room_num = random.randint(0,9)
 
 
-
-def Room_mapCr(num):
-    Roomapper = Room_list.pop(num)
-    print(Roomapper)
-Room_mapCr(Room_num)    
-
-
+def Room_maper():
+    global Room_map
+    global temp_list
+    for i in range(3):
+        for j in range(3):
+            Roomapper = temp_list.pop(random.randint(0,len(temp_list)-1))
+            Room_map[i][j] = Roomapper
+           
+Room_maper()
+print(Room_map)
 #RoomVars
 #Current room is the room the user is in currently
-Current_room = Room_list[0]
-#rooms east through west store the rooms in that position compares to the user's position 
-Room_north = Room_list[1]
-Room_east = Room_list[2]
-Room_south = Room_list[3]
-Room_west = Room_list[4]
 #test and old room system 
+player_faceing = ''
+Room_north = ''
+Room_east = ''
+Room_south = ''
+Room_west = ''
+Current_room = ''
 Current_room_old = ''
 Player_res = ''
 hp = 3
+
+
+
+def startout_sides():
+    global Room_map
+    global Current_room
+    global Room_north
+    global Room_east
+    global Room_south
+    global Room_west
+    Current_room = Room_map[1][1]
+    Room_north = Room_map[0][1]
+    Room_east = Room_map[1][2]
+    Room_south = Room_map[2][1]
+    Room_west = Room_map[1][0]
+
+
+
+startout_sides()
 
 print("You're in",Current_room)
 print("north-",Room_north)
 print("east-",Room_east)
 print("south-",Room_south)
 print("west-",Room_west)
-
 
 #replace
 while hp > 0:
@@ -103,5 +124,5 @@ while hp > 0:
 
 
 #replace/remove later
-if hp == 0:
-    print('you died')
+    if hp == 0:
+        print('you died')
