b17635c727734f77962cd44d8eeaac4075159d98
TeetoeTurtle <60118294+TeetoeTurtle@users.noreply.github.com>
2023-04-13 09:15:10 -0500

    changed how taking damage works for enemies
    joebama

--- game/engine/enemies.py
+++ game/engine/enemies.py
@@ -1,5 +1,5 @@
 class Enemy:
-    def __init__(self, type, name, description, damage, max_health, current_health, taken_damage, dead):
+    def __init__(self, type, name, description, damage, max_health, current_health, taken_damage, armor, dead):
         self.name = name
         self.type = type
         self.description = description
@@ -8,12 +8,24 @@ class Enemy:
         self.current_health = current_health
         self.taken_damage = taken_damage
         self.dead = dead
+        #defense should always be a decimal value
+        self.armor = armor
     
-    def take_damage(self, health, taken_damage, dead):
-        health = health-taken_damage
-        if health <= 0:
-            dead = True
-        taken_damage = 0
+    #damage is going to be a positive number
+    #blugeoning should be a decimal, never more than 1
+    #armor and bludgeoning are both percentage values. Armor reduces the damage the entity takes, bludgeoning reduces said damage decrease.
+    #If the bludgeoning value is greater than the armor, then 100% of damage is taken, no more
+    #When bludgeoning and armor are in play, the math works as the following:
+    #100% (the damage multiplier, represented by the int 1) is subtracted by the armor percentage, and then the bludgeoning percentage is
+    #added back to the multiplier. Example: Armor: 0.4 (40%). Bludgeoning: 0.2 (20%). 100%-40%=60%. 60%+20%=80%. 80% of damage is taken
+    def take_damage(self, damage, bludgeon=0.0):
+        if (damage < 0): 
+            if bludgeon < self.armor:
+                self.current_health -= int((1-self.armor + bludgeon) * damage)
+            elif bludgeon >= self.armor:
+                self.current_health -= damage
+        else:
+            self.current_health -= damage
     
     def give_description(self, description):
         print(description)
