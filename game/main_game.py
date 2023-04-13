0b35355372b691d1d6453cf6825c705e8deda481
datboi10 <88599867+datboi10@users.noreply.github.com>
2023-04-13 09:56:20 -0500

    Update main_game.py

--- game/main_game.py
+++ game/main_game.py
@@ -6,140 +6,140 @@
 import math, random
 
 # , room_Database, engine
-# rooms = ['cabins', 'kitchen', 'pool', 'fitness center', 'casino', 'smoking area', 'shopping center', 'the bridge',start]
+# rooms = ['cabins', 'kitchen', 'pool', 'fitness center', 'casino', 'smoking area', 'shopping center', 'the bridge', 'start']
 rooms = ['C', 'k', 'p', 'f', 'c', 's', 'S', 'B', '⌂']
 rooms_obj = {0: "C",
-             1: "k",
-             2: "p",
-             3: "f",
-             4: "c",
-             5: "s",
-             6: "S",
-             7: "B",
-             8: "⌂"}
+			 1: "k",
+			 2: "p",
+			 3: "f",
+			 4: "c",
+			 5: "s",
+			 6: "S",
+			 7: "B",
+			 8: "⌂"}
 xdist = 0
 ydist = 0
 layout = [['-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-'],
-          ['-', '-', '-', '-', '-']]
+		  ['-', '-', '-', '-', '-']]
 layout[2][2] = 6
 layout[4][2] = 7
 
 
 # classes:
 class Player:
-    def __init__(self, health, experience, inventory, strength, dexterity, resistances, immunities, effects):
-        self.hp = health
-        self.exp = experience
-        self.str = strength
-        self.dex = dexterity
-        self.inv = inventory
-        self.res = resistances
-        self.imm = immunities
-        self.eff = effects
-        self.inv = []
-        self.res = []
-        self.imm = []
-        self.eff = []
+	def __init__(self, health, experience, inventory, strength, dexterity, resistances, immunities, effects):
+		self.hp = health
+		self.exp = experience
+		self.str = strength
+		self.dex = dexterity
+		self.inv = inventory
+		self.res = resistances
+		self.imm = immunities
+		self.eff = effects
+		self.inv = []
+		self.res = []
+		self.imm = []
+		self.eff = []
 
-    def gain_item(self, item):
-        self.inv.append(item)
+	def gain_item(self, item):
+		self.inv.append(item)
 
-    def gain_res(self, resist):
-        self.inv.append(resist)
+	def gain_res(self, resist):
+		self.inv.append(resist)
 
-    def gain_imm(self, immune):
-        self.inv.append(immune)
+	def gain_imm(self, immune):
+		self.inv.append(immune)
 
-    def gain_effect(self, effect):
-        self.inv.append(effect)
+	def gain_effect(self, effect):
+		self.inv.append(effect)
 
 
 # usage: Player = Player(health, experience, inventory, strength, dexterity, intelligence, wisdom, charisma, resistances, immunities, effects)
 
 class Zombie:
-    def __init__(self, health, attack, effects, loot):
-        self.hp = health
-        self.atk = attack
-        self.eff = effects
-        self.loot = loot
-        self.loot = []
+	def __init__(self, health, attack, effects, loot):
+		self.hp = health
+		self.atk = attack
+		self.eff = effects
+		self.loot = loot
+		self.loot = []
 
-    def set_loot(self, item):
-        self.loot.append(item)
+	def set_loot(self, item):
+		self.loot.append(item)
 
 
 class Item:
-    def __init__(self, damage, effects):
-        self.dmg = damage
-        self.eff = effects
+	def __init__(self, damage, effects):
+		self.dmg = damage
+		self.eff = effects
 
 
 class Room:
-    def __init__(self, modifiers, effects, loot, zombies, resources):
-        self.mod = modifiers
-        self.eff = effects
-        self.loot = loot
-        self.zom = zombies
-        self.reso = resources
+	def __init__(self, modifiers, effects, loot, zombies, resources):
+		self.mod = modifiers
+		self.eff = effects
+		self.loot = loot
+		self.zom = zombies
+		self.reso = resources
 
 
 class ship:
-    def __init__(self, xdistance, ydistance, map, flavortext):
-        self.xdist = xdistance
-        self.ydist = ydistance
-        self.map = map
-        self.flav = flavortext
-        self.map = []
-        self.flav = []
+	def __init__(self, xdistance, ydistance, map, flavortext):
+		self.xdist = xdistance
+		self.ydist = ydistance
+		self.map = map
+		self.flav = flavortext
+		self.map = []
+		self.flav = []
 
 
 # functions:
 def display_current_map():
-    global xdist
-    global ydist
-    print(layout[ydist][xdist])
+	global xdist
+	global ydist
+	print(layout[ydist][xdist])
 
 
 def connect(x, y, loop):
-    xstart = x
-    ystart = y
-    while loop >= 1:
-        try:
-            place = random.randint(1, 4)
-            if place == 1:
-                y -= 1
-                if layout[x][y] == "-":
-                    layout[x][y] = "C"
-            if place == 2:
-                x -= 1
-                if layout[x][y] == "-":
-                    layout[x][y] = "C"
-            if place == 3:
-                x += 1
-                if layout[x][y] == "-":
-                    layout[x][y] = "C"
-
-            else:
-                y += 1
-                if layout[x][y] == "-":
-                    layout[x][y] = "C"
-            loop -= 1
-        except:
-            x = xstart
-            y = ystart
+	xstart = x
+	ystart = y
+	while loop >= 1:
+		try:
+			place = random.randint(1, 4)
+			if place == 1:
+				y -= 1
+				if layout[x][y] == "-":
+					layout[x][y] = "C"
+			if place == 2:
+				x -= 1
+				if layout[x][y] == "-":
+					layout[x][y] = "C"
+			if place == 3:
+				x += 1
+				if layout[x][y] == "-":
+					layout[x][y] = "C"
+			else:
+				y += 1
+				if layout[x][y] == "-":
+					layout[x][y] = "C"
+			loop -= 1
+		except:
+			x = xstart
+			y = ystart
+#useless, traps itself half the time, doesnt even recurse
 
 
 def map_generate():
-    xstart = 0
-    ystart = 0
-    layout[random.randint(1, 3)][random.randint(1, 3)] = '⌂'
+	xstart = random.randint(0, 4)
+	ystart = random.randint(0, 1)
+	layout[ystart][xstart] = '⌂'
 
-    for i in range(len(layout)):
-        for j in range(len(layout[i])):
-            if layout[i][j] == '⌂':
-                xstart = i
-                ystart = j
-    connect(xstart, ystart, 10)
+	#for i in range(len(layout)): #useless
+		#for j in range(len(layout[i])):
+			#if layout[i][j] == '⌂':
+				#xstart = i
+				#ystart = j
+	#connect(xstart, ystart, 100)
 
 
 # def store_user_input():
@@ -151,32 +151,35 @@ def map_generate():
 
 # variables
 for i in range(5):
-    layout[random.randint(0, len(layout) - 1)][random.randint(0, len(layout) - 1)] = rooms.pop(1)
+	(y, x) = random.randint(0, len(layout) - 1), random.randint(0, len(layout) - 1)
+	while (y, x) == (2, 2) or (4, 2):
+		(y, x) = random.randint(0, len(layout) - 1), random.randint(0, len(layout) - 1)
+	layout[y][x] = rooms.pop(1)
 
 for i in range(len(layout)):
-    for j in range(len(layout[i])):
-        if layout[i][j] == '-':
-            place = random.randint(0, 1)
-            if place == 1:
-                pass
-            # layout[i][j] = 'C'
+	for j in range(len(layout[i])):
+		if layout[i][j] == '-':
+			#place = random.randint(0, 1) #what is this for?
+			#if place == 1:
+				#pass
+			layout[i][j] = 'C'
 
 
 def human_readable_map_line(the_map_line):
-    iterator = 0
-    for cellvalue in the_map_line:
-        # repr = representation
-        repr = rooms_obj.get(cellvalue)
-        if repr is not None:
-            the_map_line[iterator] = repr
-        iterator += 1
-    return the_map_line
+	iterator = 0
+	for cellvalue in the_map_line:
+		# repr = representation
+		repr = rooms_obj.get(cellvalue)
+		if repr is not None:
+			the_map_line[iterator] = repr
+		iterator += 1
+	return the_map_line
 
 
 while 1:
-    map_generate()
-    display_current_map()
-    for i in range(len(layout)):
-        print(human_readable_map_line(layout[i]))
-    ydist = int(input('y'))
-    xdist = int(input('x'))
+	map_generate()
+	display_current_map()
+	for i in range(len(layout)):
+		print(human_readable_map_line(layout[i]))
+	ydist = int(input('y'))
+	xdist = int(input('x'))
