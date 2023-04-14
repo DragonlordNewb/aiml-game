4a991defd884f7c25490626ef12f57a86e193d30
TeetoeTurtle <60118294+TeetoeTurtle@users.noreply.github.com>
2023-04-14 09:59:48 -0500

    bludgeon and miss chance were added
    yae miko

--- game/engine/enemies.py
+++ game/engine/enemies.py
@@ -1,5 +1,6 @@
+import random
 class Enemy:
-    def __init__(self, type, name, description, damage, max_health, current_health, taken_damage, armor, dead):
+    def __init__(self, type, name, description, damage, max_health, current_health, taken_damage, miss_chance,armor=0.0):
         self.name = name
         self.type = type
         self.description = description
@@ -7,9 +8,11 @@ class Enemy:
         self.max_health = max_health
         self.current_health = current_health
         self.taken_damage = taken_damage
-        self.dead = dead
+        #miss chance is a whole number between 0-100. For example, a 33% miss chance will be 33
+        self.miss_chance=miss_chance
         #defense should always be a decimal value
         self.armor = armor
+
     
     #damage is going to be a positive number
     #blugeoning should be a decimal, never more than 1
@@ -19,13 +22,17 @@ class Enemy:
     #100% (the damage multiplier, represented by the int 1) is subtracted by the armor percentage, and then the bludgeoning percentage is
     #added back to the multiplier. Example: Armor: 0.4 (40%). Bludgeoning: 0.2 (20%). 100%-40%=60%. 60%+20%=80%. 80% of damage is taken
     def take_damage(self, damage, bludgeon=0.0):
-        if (damage < 0): 
-            if bludgeon < self.armor:
-                self.current_health -= int((1-self.armor + bludgeon) * damage)
-            elif bludgeon >= self.armor:
+        ran_num=random.randint(1,100)
+        if (ran_num>=self.miss_chance):
+            if (bludgeon < 0): 
+                if bludgeon < self.armor:
+                    self.current_health -= int((1-self.armor + bludgeon) * damage)
+                elif bludgeon >= self.armor:
+                    self.current_health -= damage
+            else:
                 self.current_health -= damage
         else:
-            self.current_health -= damage
+            print("missed")
     
     def give_description(self, description):
         print(description)
