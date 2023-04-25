98d3c8d84d434048a695e2fb9e939dc2bf910b41
Noddley <102306461+Noddley@users.noreply.github.com>
2023-04-25 09:05:00 -0500

    Update game_Run.py

--- game/game_Run.py
+++ game/game_Run.py
@@ -18,12 +18,12 @@ start_state = True
 def print_slow(text):
     for c in text:
         print(c, end='', flush=True)
-        time.sleep(0.05)
+        time.sleep(0.0)
 
 def room_selsction():
 
     if current_room == 'cabin':
-        print_slow(T.move_xy(10,10)('“You poke your head out of the door very slowly, allowing the hallway running from the east to west to become visible. All of the other doors are shut and the silence is only broken by the security guard at the end of the east hallway quite a ways down. His head swinging back and forth in an agitated motion. He looks around the corner and immediately turns and tries to run. He trips and falls on his knees, stumbling to move forward. His hand goes to the pepper spray at his side and he fumbles with the grip. In a moment of particularly bad luck he drops it on the floor just out of his reach as a grotesque, limping… thing steps around the corner. It steps on the spray cracking it into uselessness and reaches for him. You pull the door closed and not a moment later hear the sudden scream of the security guard, then, silence. What would you like to do?”'))
+        print_slow(T.move_xy(20,20)('“You poke your head out of the door very slowly, allowing the hallway running from the east to west to become visible. All of the other doors are shut and the silence is only broken by the security guard at the end of the east hallway quite a ways down. His head swinging back and forth in an agitated motion. He looks around the corner and immediately turns and tries to run. He trips and falls on his knees, stumbling to move forward. His hand goes to the pepper spray at his side and he fumbles with the grip. In a moment of particularly bad luck he drops it on the floor just out of his reach as a grotesque, limping… thing steps around the corner. It steps on the spray cracking it into uselessness and reaches for him. You pull the door closed and not a moment later hear the sudden scream of the security guard, then, silence. What would you like to do?”'))
 
 #room_selsction()
 
@@ -38,21 +38,22 @@ def update():
 def health_System():
     global health
     print(T.clear)
-    while T.inkey(timeout=0.02) != 'q':
-        dashConvert = int(maxHealth/healthspace)            
-        currentDash = int(health/dashConvert)              
-        remainingHealth = healthspace - currentDash      
-        healthDisplay = T.on_green(' ') * currentDash                 
-        remainingDisplay = ' ' * remainingHealth             
-        percent = str(int((health/maxHealth)*100)) + "%" 
-        bar = healthDisplay + remainingDisplay     
-        #print(T.on_green(T.move_xy(10, 10) + healthDisplay + remainingDisplay + T.clear_eol))
-        print((T.move_xy(0, 100) + bar))
-        print(T.move_xy(0,100)("              " + percent))
-        room_selsction()
-        time.sleep(1)
-        health -= 1
-        print(T.clear)
+    room_selsction()
+    #while T.inkey(timeout=0.02) != 'q':
+    dashConvert = int(maxHealth/healthspace)            
+    currentDash = int(health/dashConvert)              
+    remainingHealth = healthspace - currentDash      
+    healthDisplay = T.on_green(' ') * currentDash                 
+    remainingDisplay = ' ' * remainingHealth             
+    percent = str(int((health/maxHealth)*100)) + "%" 
+    bar = healthDisplay + remainingDisplay     
+    #print(T.on_green(T.move_xy(10, 10) + healthDisplay + remainingDisplay + T.clear_eol))
+    print((T.move_xy(0, 100) + bar))
+    print(T.move_xy(0,100)("              " + percent))
+    
+    time.sleep(1)
+    health -= 1
+    print(T.clear)
 
 
 def porgram_start():
