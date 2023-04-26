b0b7f7f3abbf8ec854904cedc97768cd15d21c31
Noddley <102306461+Noddley@users.noreply.github.com>
2023-04-26 10:19:01 -0500

    Update game_Run.py
    Stuff is still broken I need to work on importing everything so that it runs well

--- game/game_Run.py
+++ game/game_Run.py
@@ -1,8 +1,8 @@
 import blessed
 import time
 import keyboard
-#from room_selector import Current_room
-
+import engine 
+from engine import rooms
 #from save import health
 T = blessed.Terminal()
 health = 100    
@@ -11,29 +11,33 @@ healthspace = 30
 sheild = 100
 maxSheild = 100
 sheildSpace = 25
-start_state = True
+current_room = 'cabin'
 text_state = 0
-Current_room = 'cabin'
+start_state = True
+
 
 
 def print_slow(text):
     for c in text:
         print(c, end='', flush=True)
-        time.sleep(0.03)
+        time.sleep(0.02)
 
 def room_selsction():
     global text_state
-    if Current_room == 'cabin' and text_state == 0:
-        print(T.move_xy(5,15)('“You poke your head out of the door very slowly, allowing the hallway running from the east to west to become visible. All of the other doors are shut and the silence is only broken by the security guard at the end of the east hallway quite a ways down. His head swinging back and forth in an agitated motion. He looks around the corner and immediately turns and tries to run. He trips and falls on his knees, stumbling to move forward. His hand goes to the pepper spray at his side and he fumbles with the grip. In a moment of particularly bad luck he drops it on the floor just out of his reach as a grotesque, limping… thing steps around the corner. It steps on the spray cracking it into uselessness and reaches for him. You pull the door closed and not a moment later hear the sudden scream of the security guard, then, silence. What would you like to do?”'))
-        text_state += 1
-    
+    if current_room == 'cabin' and text_state == 0:
+        print_slow(T.move_xy(20,20)('“You poke your head out of the door very slowly, allowing the hallway running from the east to west to become visible. All of the other doors are shut and the silence is only broken by the security guard at the end of the east hallway quite a ways down. His head swinging back and forth in an agitated motion. He looks around the corner and immediately turns and tries to run. He trips and falls on his knees, stumbling to move forward. His hand goes to the pepper spray at his side and he fumbles with the grip. In a moment of particularly bad luck he drops it on the floor just out of his reach as a grotesque, limping… thing steps around the corner. It steps on the spray cracking it into uselessness and reaches for him. You pull the door closed and not a moment later hear the sudden scream of the security guard, then, silence. What would you like to do?”'))
+        text_state = 1
+    elif text_state <= 1 and current_room == 'cabin':
+        print(T.move_xy(20,20)('“You poke your head out of the door very slowly, allowing the hallway running from the east to west to become visible. All of the other doors are shut and the silence is only broken by the security guard at the end of the east hallway quite a ways down. His head swinging back and forth in an agitated motion. He looks around the corner and immediately turns and tries to run. He trips and falls on his knees, stumbling to move forward. His hand goes to the pepper spray at his side and he fumbles with the grip. In a moment of particularly bad luck he drops it on the floor just out of his reach as a grotesque, limping… thing steps around the corner. It steps on the spray cracking it into uselessness and reaches for him. You pull the door closed and not a moment later hear the sudden scream of the security guard, then, silence. What would you like to do?”'))
+
 
 #room_selsction()
 
 
 def update():
     if health >= 100:
-        health_System()
+        print(T.clear)
+        health_System
 
 
 
@@ -42,7 +46,6 @@ def health_System():
     global health
     print(T.clear)
     while T.inkey(timeout=0.02) != 'q':
-        T.enter_fullscreen
         dashConvert = int(maxHealth/healthspace)            
         currentDash = int(health/dashConvert)              
         remainingHealth = healthspace - currentDash      
@@ -51,30 +54,31 @@ def health_System():
         percent = str(int((health/maxHealth)*100)) + "%" 
         bar = healthDisplay + remainingDisplay     
         #print(T.on_green(T.move_xy(10, 10) + healthDisplay + remainingDisplay + T.clear_eol))
-        Test1 = print((T.move_xy(0, 100) + bar))
-        Test = print(T.move_xy(0,100)("              " + percent))
+        print((T.move_xy(0, 100) + bar))
+        print(T.move_xy(0,100)("              " + percent))
         room_selsction()
-        time.sleep(2)
-        print('/b' + T.clear)
+        time.sleep(1)
         health -= 1
+        print_slow(T.clear)
         
-        
-        
+
+
 def porgram_start():
     health_System()
     update()
-    room_selsction()
+    #room_selsction()
 
 
 def start():
     while start_state ==  True:
         print(T.home + T.clear + T.move_y(T.height // 2))
-        print(T.black_on_blue(T.center('press any key to start.')))
+        print(T.black_on_blue(T.center('press any key to continue.')))
         with T.cbreak(), T.hidden_cursor():
             inp = T.inkey()
         #print(T.move_down(2) + 'You pressed ' + T.bold(repr(inp)))
         #start_state = False
-        porgram_start()
+        #porgram_start()
+        print(rooms)
         break
 
 
